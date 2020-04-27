b6b3c01edb0099bcc1df13fdbff3b3551fc7fd57
Zabehalin <z380966645657@gmail.com>
2020-04-27 13:13:23 +0300

    Telelgram BOT

--- PostgreSQL/lib/db.py
+++ PostgreSQL/lib/db.py
@@ -1,16 +1,19 @@
 import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+if __name__ == "__main__":
+    connect_to_db,
 
 
 def connect_to_db():
-    conn = psycopg2.connect(database="postgres", user="postgres",
+    conn = psycopg2.connect(database="test", user="postgres",
                             password="root", host="127.0.0.1", port="5432")
-# =============================== CONECT ========================================
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
     cursor = conn.cursor()
-    name_Table = "Tests"
-    sqlCreateTable = "CREATE TABLE " + name_Table + \
-        " (id bigint, title varchar(128), summary varchar(256), story text);"
-    cursor.execute(sqlCreateTable)
+    #sqll = "CREATE DATABASE test"
+    sql = "CREATE TABLE IF NOT EXISTS account(cardnumber serial PRIMARY KEY,name VARCHAR(50) NOT NULL,surname VARCHAR(50) NOT NULL,amount FLOAT(10) NOT NULL,currancy VARCHAR (10) NOT NULL)"
+    cursor.execute(sql)
     conn.commit()
-    conn.close()
     print("Working")
+    return conn
